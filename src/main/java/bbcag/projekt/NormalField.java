d924d05fa269678803c9f25d6b3341a0b76dfacb
bhugis <sven.hugi@bbcag.ch>
2019-05-02 13:55:55 +0200

    Creating Project save 3

--- src/main/java/bbcag/projekt/NormalField.java
+++ src/main/java/bbcag/projekt/NormalField.java
@@ -1,4 +1,25 @@
 package bbcag.projekt;
 
-public class NormalField {
+public class NormalField extends Field{
+    private short Worth;
+    private byte Hotel = 0;
+    private int[] Rent;
+
+    public NormalField(String name, short worth, int[] rent){
+        this.name = name;
+        this.Worth = worth;
+        this.Rent = rent;
+    }
+    @Override
+    public void stepingOnIt(Player player) {
+
+    }
+
+    public int getRent() {
+        return Rent[Hotel];
+    }
+
+    private void buy (Player player){
+
+    }
 }
