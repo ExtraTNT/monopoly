dba03e917a7a1f1a6429444a35faf23ab6f7666a
shu114015 <shu114015@iet-gibb.ch>
2019-09-18 21:25:40 +0200

    clean up

--- src/main/java/bbcag/projekt/player/Player.java
+++ src/main/java/bbcag/projekt/player/Player.java
@@ -8,62 +8,46 @@ public class Player {
     private byte remainingDaysInPrison;
     private int count = 0;
 
-    /* This isn't being used yet
-    private int pach = 0;
-    */
-
     public Player(String name, String color) {
         this.name = name;
         this.color = color;
     }
-
     public boolean isDeath() {
         return (accountBalance < 0);
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getAccountBalance() {
         return accountBalance;
     }
-
     public void setAccountBalance(int accountBalance) {
         this.accountBalance = accountBalance;
     }
-
     public byte getPosition() {
         return position;
     }
-
     public void setPosition(byte position) {
         this.position = position;
     }
-
     public byte getRemainingDaysInPrison() {
         return remainingDaysInPrison;
     }
-
     public void setRemainingDaysInPrison(byte remainingDaysInPrison) {
         this.remainingDaysInPrison = remainingDaysInPrison;
     }
     public String getColor() {
         return color;
     }
-
     public int getCount() {
         return count;
     }
-
     public void setCount(int count) {
         this.count = count;
     }
-
     @Override
     public String toString() {
         return name;
